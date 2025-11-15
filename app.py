6435bc81390864b23aa099994a723f38c3f12d7a
yuhyeogjeong3-prog <yuhyeogjeong3@gmail.com>
2025-11-15 11:54:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,36 +9,18 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Brick Breaker")
+pygame.display.set_caption("Dodge Game")
 
 # 색상 정의
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
-# 게임 오브젝트 클래스
-
-# 막대기 (플레이어) 클래스
-class Paddle(pygame.sprite.Sprite):
+# 플레이어(캐릭터) 클래스
+class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((100, 20))
+        self.image = pygame.Surface((50, 50))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        self.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT - 30)
-        self.speed = 10
-
-    def update(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and self.rect.left > 0:
-            self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
-            self.rect.x += self.speed
-
-# 공 (볼) 클래스
-class Ball(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        s
